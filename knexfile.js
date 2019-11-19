22fd1b0a06c221766c3f73bda151d32db71333c2
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-11-19 09:00:41 -0800

    using sqlite for prod

--- knexfile.js
+++ knexfile.js
@@ -24,9 +24,21 @@ module.exports = {
     seeds: { directory: './database/seeds' },
   },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+  // production: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL,
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: './database/migrations',
+  //     tableName: 'dbmigrations',
+  //   },
+  //   seeds: { directory: './database/seeds' },
+  // },
+
+  development: {
+    client: 'sqlite3',
+    connection: { 
+      filename: './database/auth.db3'},
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
