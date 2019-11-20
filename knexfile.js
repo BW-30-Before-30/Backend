a3ab4f21c42f0fa3e6f9e01928c9113c38afed83
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2019-11-20 09:33:22 -0800

    knex file and db config

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,13 @@
 // Update with your config settings.
+require('dotenv').config();
 
+const localPg = {
+	host: 'localhost',
+	database: process.env.DB_NAME,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
 module.exports = {
 	development: {
 		client: 'sqlite3',
@@ -33,11 +41,7 @@ module.exports = {
 
 	production: {
 		client: 'pg',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password'
-		},
+		connection: dbConnection,
 		pool: {
 			min: 2,
 			max: 10
